da4c389b1ba544e0469c5aabe3836da1d62307d0
Ahani_Samira <Ahani29@gmail.com>
2024-08-14 15:46:24 +0330

    * Add about_us view * Complete the about_us.html template

--- charity/about_us/views.py
+++ charity/about_us/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+from accounts.models import User
+
 def about_us(request):
-    # View for about-us page
-    ...
+    charities_list = User.objects.all()
+    template = loader.get_template("about_us.html")
+    context = {
+        "charities_list": charities_list,
+    }
+    return HttpResponse(template.render(context, request))
